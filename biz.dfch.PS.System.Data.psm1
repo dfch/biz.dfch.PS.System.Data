e814a38e4c77f21da9559f7ef2fc693cd55e3aa7
tgdriro5 <ronald.rink@d-fens.net>
2014-11-26 08:38:25 +0100

    2014-11-26; rrink; ADD: modele removal of ScriptsToProcess, http://d-fens.ch/2014/11/26/bug-powershell-scripts-in-scriptstoprocess-attribute-appear-as-loaded-modules/

--- biz.dfch.PS.System.Data.psm1
+++ biz.dfch.PS.System.Data.psm1
@@ -20,6 +20,21 @@ if($true -ne (Test-Path variable:$($mvar))) {
 } # if()
 Export-ModuleMember -Variable $mvar;
 
+[string] $ManifestFile = '{0}.psd1' -f (Get-Item $PSCommandPath).BaseName;
+$ManifestPathAndFile = Join-Path -Path $PSScriptRoot -ChildPath $ManifestFile;
+if( Test-Path -Path $ManifestPathAndFile)
+{
+	$Manifest = (Get-Content -raw $ManifestPathAndFile) | iex;
+	foreach( $ScriptToProcess in $Manifest.ScriptsToProcess) 
+	{ 
+		$ModuleToRemove = (Get-Item (Join-Path -Path $PSScriptRoot -ChildPath $ScriptToProcess)).BaseName;
+		if(Get-Module $ModuleToRemove)
+		{ 
+			Remove-Module $ModuleToRemove -ErrorAction:SilentlyContinue;
+		}
+	}
+}
+
 <#
  # ########################################
  # Version history
